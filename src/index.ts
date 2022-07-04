690f8b870ea592a42982f11fc03c4344fce17cf3
nahsooyeon <nahsooyeon@gmail.com>
2022-07-04 23:28:00 +0900

    🔨 수정: starting server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import Koa from "koa";
+import Koa, { Context, Next } from 'koa';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hellow World";
+/* app.env default:process.NODE_ENV || 'development' */
+
+/* proxy */
+app.proxy = true;
+
+/* set cookie keys */
+app.keys = ['keygrip type string'];
+
+/* CONTEXT:  */
+
+app.use(async (ctx: Context, next: Next) => {
+  await next();
+  const rt = ctx.response.get('X-Reponse-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
-app.listen(3000);
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
